f945c07d6297ffb7e173ec49f8d89920e46b6e7c
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-04-16 23:52:38 +0700

    query parametes

--- 1-hello-coders-tokyo/index.js
+++ 1-hello-coders-tokyo/index.js
@@ -4,21 +4,39 @@ var app = express();
 var port = 3000;
 
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
+
+var users = [
+		{id:1, name: "trung"},
+		{id:2, name: "nam"}
+	];
 
 app.get('/', function(req,res) {
 	res.render('index', { // tham số thứ nhất là path này tính từ ./views
 		"name" : "Dang Trung" // tham số thứ hai là object
 	});
 })
-
+ 
 app.get('/user', function(req,res) {
 	res.render('users/index', {
-		users: [
-			{id:1, name: "trung"},
-			{id:2, name: "nam"}
-		]
+		users: users
 	});
+})
+
+app.get('/user/search', function(req, res) {
+	var q = req.query.q;
+	var machedUsers = users.filter(function(user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) ==! -1
+		// nếu q nằm trong user.name thì sẽ trả về giá trị > -1
+		// nếu q không nằm trong user.name thì sẽ trả về giá trị < -1
+	})
+	//render
+	res.render('users/index', {
+		users: machedUsers
+	});
+
+	var search = document.getElementById('search');
+	search.value = q;
 })
 
 // HÀM GET LÀ LẤY require và trả về response
