22a0400f0dbb5588dca2350e44e68b74f0dd1f6c
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-04-16 22:38:25 +0700

    template engines, pug, SPA

--- 1-hello-coders-tokyo/index.js
+++ 1-hello-coders-tokyo/index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var app = express();
+
 var port = 3000;
 
-app.get('/', function(require,response) {
-	response.send('hello coders.tokyo');
+app.set('view engine', 'pug');
+app.set('views', './views')
+
+app.get('/', function(req,res) {
+	res.render('index', { // tham số thứ nhất là path này tính từ ./views
+		"name" : "Dang Trung" // tham số thứ hai là object
+	});
 })
 
-app.get('/user', function(require,response) {
-	response.send('<h1>hello coders.tokyo</h1>');
+app.get('/user', function(req,res) {
+	res.render('users/index', {
+		users: [
+			{id:1, name: "trung"},
+			{id:2, name: "nam"}
+		]
+	});
 })
 
 // HÀM GET LÀ LẤY require và trả về response
-
 app.listen(port, function(){
 	console.log('server listening on ' + port);
 })
