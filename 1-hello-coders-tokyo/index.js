d19b490e7085332f31368f8b6f4dee1d5175bfe5
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-04-19 09:59:30 +0700

    lowdb, basic database

--- 1-hello-coders-tokyo/index.js
+++ 1-hello-coders-tokyo/index.js
@@ -1,35 +1,39 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
 var port = 3000;
+
+var low = require('lowdb')
+var FileSync = require('lowdb/adapters/FileSync')
+
+var adapter = new FileSync('db.json')
+var db = low(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write()
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-var users = [
-		{id:1, name: "trung"},
-		{id:2, name: "nam"},
-		{id:3, name: "hung"}
-	];
-
 app.get('/', function(req,res) {
 	res.render('index', { // tham số thứ nhất là path này tính từ ./views
 		"name" : "Dang Trung" // tham số thứ hai là object
 	});
 })
- 
+
 app.get('/users', function(req,res) {
 	res.render('users/index', {
-		users: users
+		users: db.get('users').value() // đọc giá trị trong thàng defalets trên kia ra 
 	});
 })
 
 app.get('/users/search', function(req, res) {
 	var q = req.query.q;
-	var machedUsers = users.filter(function(user) {
+	var machedUsers = db.get('users').value().filter(function(user) {
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) ==! -1
 		// nếu q nằm trong user.name thì sẽ trả về giá trị > -1
 		// nếu q không nằm trong user.name thì sẽ trả về giá trị < -1
@@ -40,22 +44,20 @@ app.get('/users/search', function(req, res) {
 	});
 })
 
+// HÀM GET LÀ LẤY require và trả về response
 app.get('/users/create', function(req, res) {
 	res.render('users/create')
 })
 
-// tạo một cái en poi để có thể trả lời được khi nhận một các request như vậy
+// tạo một cái en poi để có thể trả lời được khi nhận một các request 
 app.post('/users/create', function(req, res) {
-	users.push(req.body);
-
+	// đọc và ghi vào db.json
+	db.get('users').push(req.body).write();
 	// cho người dùng quay về trang users
 	res.redirect("/users") 
-
-	console.log(req.body);
-	console.log(users);
 })
 
-// HÀM GET LÀ LẤY require và trả về response
+
 app.listen(port, function(){
 	console.log('server listening on ' + port);
 })
