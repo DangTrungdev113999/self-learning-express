517bf9f776cd9eaeed83079f58a81bf1ab7356e4
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-04-20 20:14:52 +0700

    controller(mvc), template layout, static file

--- 1-hello-coders-tokyo/index.js
+++ 1-hello-coders-tokyo/index.js
@@ -4,11 +4,14 @@ var userRoute = require('./routes/route.user')
 var app = express();
 
 var port = 3000;
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public')) // static file
 
 app.get('/', function(req,res) {
 	res.render('index', { // tham số thứ nhất là path này tính từ ./views
--- 1-hello-coders-tokyo/routes/route.user.js
+++ 1-hello-coders-tokyo/routes/route.user.js
@@ -1,54 +1,21 @@
 var express = require('express');
-var shortid = require('shortid');
+var controller = require('../controllers/user.controller.js')
 
-
-var db = require('../db') // do không nằm chung folder nên phải ra ngoài folder
 
 var router = express.Router();
 
-router.get('/', function(req,res) { 
-	res.render('users/index', {
-		users: db.get('users').value() // đọc giá trị trong thàng defalets trên kia ra 
-	});
-})
+router.get('/', controller.index)
 
 // cái routing này để tìm kiếm
-router.get('/search', function(req, res) {  // đường dẫn bắt đầu bằng users nội tại
-	var q = req.query.q;
-	var machedUsers = db.get('users').value().filter(function(user) {
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) ==! -1
-		// nếu q nằm trong user.name thì sẽ trả về giá trị > -1
-		// nếu q không nằm trong user.name thì sẽ trả về giá trị < -1
-	})
-	//render
-	res.render('users/index', {
-		users: machedUsers
-	});
-})
+router.get('/search', controller.search)
 
 // HÀM GET LÀ LẤY require và trả về response
-router.get('/create', function(req, res) {
-	res.render('users/create')
-})
+router.get('/create', controller.create)
 
 // cả thàng này gọi là routing, cái này là dyamic routing
-router.get('/:id', function(req, res) {
-	var id = req.params.id // params khác với query
-	var user = db.get('users').find({ id: id }).value()
-
-	// sau đó render ra
-	res.render('users/view', {
-		user: user
-	})
-})
+router.get('/:id', controller.get)
 
 // tạo một cái en poi để có thể trả lời được khi nhận một các request 
-router.post('/create', function(req, res) {
-	req.body.id = shortid.generate();
-	// đọc và ghi vào db.json
-	db.get('users').push(req.body).write();
-	// cho người dùng quay về trang users
-	res.redirect("/users") 
-})
+router.post('/create', controller.postCreate)
 
 module.exports = router;
